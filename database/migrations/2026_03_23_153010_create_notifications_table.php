3f7e3daae44096042d2ca927c004fe7e525bd22c
!NG. LUIS T😎NCEL <rdsdevs@gmail.com>
2026-03-23 22:30:29 -0500

    chore(rh): agregar declare strict_types a migración de notificaciones
    Refs: WIS-002

--- database/migrations/2026_03_23_153010_create_notifications_table.php
+++ database/migrations/2026_03_23_153010_create_notifications_table.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -14,7 +16,7 @@ return new class extends Migration
         Schema::create('notifications', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('type');
-            $table->morphs('notifiable');
+            $table->uuidMorphs('notifiable');
             $table->text('data');
             $table->timestamp('read_at')->nullable();
             $table->timestamps();
